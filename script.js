a039a17a6063db27c2e914650a90dd2308f87632
nathanim1919 <nathanim2tadele@gmail.com>
2022-10-01 20:20:44 -0700

    making code clean

--- script.js
+++ script.js
@@ -1,107 +1,104 @@
-let X_CLASS = 'x';
-let CIRCLE_CLASS = 'circle';
-let circleTurn = false;
-const board = document.querySelector('#board');
-const winner = document.querySelector('.showWinner');
-const winnerInfo = document.querySelector('.show');
-const restart = document.querySelector('.restart');
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
+const getDom = (()=>{
 
-const humanPlayMode = document.querySelector('.human-play-mode');
-const aiPlayMode = document.querySelector('.ai-play-mode');
+    const board = document.querySelector('#board');
+    const winner = document.querySelector('.showWinner');
+    const winnerInfo = document.querySelector('.show');
+    const restart = document.querySelector('.restart');
+    const player1 = document.getElementById('player1');
+    const player2 = document.getElementById('player2');
+    const humanPlayMode = document.querySelector('.human-play-mode');
+    const aiPlayMode = document.querySelector('.ai-play-mode');
+    const intro_page = document.querySelector('.intro');
+    const player1_name = document.querySelector('.p1-name')
+    const player2_name = document.querySelector('.p2-name')
+    const p1_sign = document.querySelector('.p1-sign');
+    const p2_sign = document.querySelector('.p2-sign');
+    const cells = document.querySelectorAll('[data-cell]');
+    const dislayWinner = document.querySelector('.dislayWinner');
+    const goback = document.querySelector('.goback');
+    const p1_info = document.querySelector('.p1');
+    const p2_info = document.querySelector('.p2');
+    let humanMode = false;
+    let aiMode = false;
 
-const intro_page = document.querySelector('.intro');
-const player1_name = document.querySelector('.p1-name')
-const player2_name = document.querySelector('.p2-name')
-const p1_sign = document.querySelector('.p1-sign');
-const p2_sign = document.querySelector('.p2-sign');
-const cells = document.querySelectorAll('[data-cell]');
-const dislayWinner = document.querySelector('.dislayWinner');
-const goback = document.querySelector('.goback');
+    return{
+      humanMode,
+      aiMode,
+      board,
+      winner,winnerInfo,
+      restart,
+      player1,
+      player2,
+      humanPlayMode,
+      aiPlayMode,
+      intro_page,
+      player1_name,
+      player2_name,
+      p1_sign,
+      p2_sign,
+      cells,
+      dislayWinner,
+      goback,
+      p1_info,
+      p2_info
+    }
 
-const p1_info = document.querySelector('.p1');
-const p2_info = document.querySelector('.p2');
-
-
-var humanMode = false;
-var aiMode = false;
+})();
 
 
 // event listners
-humanPlayMode.addEventListener('click',startHumanMode);
-aiPlayMode.addEventListener('click',startAiMode);
+getDom.humanPlayMode.addEventListener('click',startHumanMode);
+getDom.aiPlayMode.addEventListener('click',startAiMode);
 
 
-
-// functions
+//global functions
 function startHumanMode() {
-    if (player1.value != '' && player2.value != '') {
-        intro_page.classList.add('close-page');
-        player1_name.textContent = player1.value;
-        player2_name.textContent = player2.value;
-        humanMode = true;
-        aiMode = false;
+    if (getDom.player1.value != '' && getDom.player2.value != '') {
+        getDom.intro_page.classList.add('close-page');
+        getDom.player1_name.textContent = getDom.player1.value;
+        getDom.player2_name.textContent = getDom.player2.value;
+        getDom.humanMode = true;
+        getDom.aiMode = false;
         game.round = 1;
 
     }
 }
 
-
 function startAiMode() {
-        player1_name.textContent =  'YOU';
-        player2_name.textContent = 'COMPUTER';
-        intro_page.classList.add('close-page');
-        aiMode = true;
-        humanMode = false;
+        getDom.player1_name.textContent =  'YOU';
+        getDom.player2_name.textContent = 'COMPUTER';
+        getDom.intro_page.classList.add('close-page');
+        getDom.aiMode = true;
+        getDom.humanMode = false;
         game.round = 1;
     }
-
-
-
-
 
 
 // player factorial functions
 const Player = (sign) => {
       this.sign = sign;
-
       const getSign = () => {
         return sign;
       };
-
       return { 
         getSign
       };
   };
 
 
-// gameboard
+// gameboard IIFE 
   const gameboard = (()=>{
-
       let board = ['','','','','','','','',''];
-      
       const setField = (index,sign)=>{
-          if (index > board.length || board[index] != '') {
-            return;
-          }
+          if (index > board.length || board[index] != '') return;
           board[index] = sign;
           displayController.updateBoard();
+          game.isGameOver();
       };
-
-
-
-
-
       const getfield = (index)=>{
-          if (index>board.length) {
-            return
-          }
-
+          if (index>board.length) return;
           return board[index];
       };
-
-
       const reset = () =>{
           for (let i = 0; i < board.length; i++) {
             board[i] = '';
@@ -109,10 +106,7 @@ const Player = (sign) => {
           }
       };
 
-
-
       return{
-
           setField,
           getfield,
           reset,
@@ -125,15 +119,10 @@ const Player = (sign) => {
 
   // gamecontroller
   const game = (()=>{
-
-
       const palyerX = Player('X');
       const playerO = Player('O');
-
-      p1_sign.textContent = `--${palyerX.getSign()}--`;
-      p2_sign.textContent = `--${playerO.getSign()}--`;
-
-
+      getDom.p1_sign.textContent = `--${palyerX.getSign()}--`;
+      getDom.p2_sign.textContent = `--${playerO.getSign()}--`;
       let round  = 1;
       let POSSIBLE_WINNING_COMBINATIONS = [
         [0,1,2],
@@ -146,127 +135,70 @@ const Player = (sign) => {
         [2,4,6],
       ];
 
+      // minimax algorithm implimentation
+      const minimax = (board,depth,isMax) =>{
 
+      }
+
+
+      // play with AI
       const aiPlayRound = (fild)=>{
-
-              const you = Player('X'); 
-              const ai = Player('O');
+             
               let emptyfilds = [];
 
-              // let round = 1;
-
-               //  get current player sign
-              const getCurrentPlayerSign = ()=>{
-                return round % 2 === 1?you.getSign():ai.getSign();
-              }
-            
-
-
+              // get all available empty fields
               const getEmptyFields = ()=>{
                 for (let i = 0; i < 9; i++) {
                   if (gameboard.getfield(i) == '') {
-                    emptyfilds.push(i);
-                  }
-                  
-                }
-                  
+                        emptyfilds.push(i);
+                     } 
+                 }   
               }
-            
-            
+
               const aiMove =()=>{
-                  let random = Math.floor(Math.random()*emptyfilds.length);
-                  gameboard.setField(emptyfilds[random],getCurrentPlayerSign());
+                  // let bestscore = -Infinity;
+                  // let score = minimax(emptyfilds());
 
-                  if (winning()) {
-                    dislayWinner.textContent  = `--${getCurrentPlayerSign()}  Wins`;
-                    winner.classList.add('display');
-                    console.log('o is the winner')
-                    return;
-                  }
-                  
-    
-                  else if(draw()){
-                    winner.classList.add('display');
-                    dislayWinner.textContent = `--its a draw--`;
-                    round = 1;
-                  }
-
+                  let randomMove = Math.floor(Math.random()*emptyfilds.length);
+                  gameboard.setField(emptyfilds[randomMove],getCurrentPlayerSign());
+                  isGameOver();
                   round++;
               }
-            
-              gameboard.setField(fild,getCurrentPlayerSign());
-
-              if (winning()) {
-                dislayWinner.textContent  = `--${getCurrentPlayerSign()}  Wins`;
-                winner.classList.add('display');
-                console.log('x is the winner')
-                return;
-              }
               
-
-              else if(draw()){
-                winner.classList.add('display');
-                dislayWinner.textContent = `--its a draw--`;
-                round = 1;
-              }
-
-
-
-
-              round++;
+              gameboard.setField(fild,getCurrentPlayerSign());
+              // isGameOver(); 
               getEmptyFields();
-              setTimeout(aiMove,300);
-             
+              round++;
+              setTimeout(aiMove,500);   
+      }
 
-            
-             displayController.updateBoard();
 
-          
+
+      // show whose turn is now!
+      const displayTurn = ()=>{
+
+        if (getCurrentPlayerSign() == 'X') {
+          getDom.p1_info.classList.remove('display-info');
+          getDom.p2_info.classList.add('display-info');
+        }
+
+        if (getCurrentPlayerSign() == 'O') {
+          getDom.p1_info.classList.add('display-info');
+          getDom.p2_info.classList.remove('display-info');
+        }
       }
 
 
 // two player mode
       const playRound = (fild)=>{
-
               gameboard.setField(fild,getCurrentPlayerSign());
+              displayTurn();
               displayController.updateBoard();
+              // isGameOver();
+              round++;      
+        }
 
-              
-
-            
-              if (getCurrentPlayerSign() == 'X') {
-                p1_info.classList.remove('display-info');
-                p2_info.classList.add('display-info');
-              }
-
-              if (getCurrentPlayerSign() == 'O') {
-                p1_info.classList.add('display-info');
-                p2_info.classList.remove('display-info');
-              }
-
-                
-              if (winning()) {
-                dislayWinner.textContent  = `--${getCurrentPlayerSign()}  Wins`;
-                winner.classList.add('display');
-              }
-              
-
-                else if(draw()){
-                  winner.classList.add('display');
-                  dislayWinner.textContent = `--its a draw--`;
-                  round = 1;
-                }
-                
-                round++;
         
-        }
-
-        //  get current player sign
-        const getCurrentPlayerSign = ()=>{
-            return round % 2 === 1?palyerX.getSign():playerO.getSign();
-        }
-
-
 
         // check possible winning conditions
         const winning = ()=>{
@@ -277,91 +209,93 @@ const Player = (sign) => {
               });
         }
 
-        
         // check for draw
         const draw = ()=>{
-          return Array.from(cells).every(cell=>{
+          return Array.from(getDom.cells).every(cell=>{
             return (cell.textContent != '');
           });   
 
          }
 
+
+         //  get current player sign
+        const getCurrentPlayerSign = ()=>{
+          return round % 2 === 1?palyerX.getSign():playerO.getSign();
+        }
+
+        // is game over(win or draw)
+         const isGameOver = ()=>{
+
+            if (winning()) {
+              getDom.dislayWinner.textContent  = `--${getCurrentPlayerSign()}  Wins`;
+              getDom.winner.classList.add('display');
+             
+              if(getCurrentPlayerSign() == 'X'){
+                   return -1;
+              }else if(getCurrentPlayerSign() == 'O'){
+                   return 11;
+              }
+             
+
+            } else if(draw()){
+
+                  getDom.winner.classList.add('display');
+                  getDom.dislayWinner.textContent = `--its a draw--`;
+                  return 0;
+                  
+            }
+
+
+        }
+      
         // restart game
         const restartGame =()=>{
           gameboard.reset();
           displayController.updateBoard();
-          winner.classList.remove('display');
+          getDom.winner.classList.remove('display');
           round = 1;
         }
 
         const returnMenu = ()=>{
           gameboard.reset();
           displayController.updateBoard();
-          winner.classList.remove('display');
-          intro_page.classList.remove('close-page');
+          getDom.winner.classList.remove('display');
+          getDom.intro_page.classList.remove('close-page');
           round = 1;
         }
-
-        if (winning()) {
-          winner.classList.add('display');
-          winnerInfo.textContent = `--${getCurrentPlayerSign()}--`;
-          round = 1;
-        }
-
-        
-
         return{
-
             playRound,
             getCurrentPlayerSign,
             restartGame,
             returnMenu,
-            aiPlayRound,
-            winning,
-            draw,
+            aiPlayRound,isGameOver,
 
         }
   })()
 
-
-
   // displayfields
   const displayController = (()=>{
-
-        restart.addEventListener('click',game.restartGame);
-        goback.addEventListener('click',game.returnMenu);
-
-       
-        cells.forEach((cell,i) =>{ 
+        getDom.restart.addEventListener('click',game.restartGame);
+        getDom.goback.addEventListener('click',game.returnMenu);
+        getDom.cells.forEach((cell,i) =>{ 
             cell.addEventListener('click',function(e){
               if(cell.textContent != '') return;
-              if (humanMode) {
+              if (getDom.humanMode) {
                 game.playRound(i);
               
-              }else if(aiMode){
+              }else if(getDom.aiMode){
                 game.aiPlayRound(i);
               }
-            });
-          
+            });  
         });
-        
-
         const updateBoard = () =>{
-          cells.forEach((cell,i) =>{
+          getDom.cells.forEach((cell,i) =>{
             cell.textContent = gameboard.getfield(i);
           })    
-
         }
+
     return{
       updateBoard,
     }
 
   })();
-
-
-
-
-
-
-
-
